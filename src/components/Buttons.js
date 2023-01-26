3ee1d16370c29bb8650df3bd56cbd0dbf13a9719
unknown <81180379+Beka-lomsa@users.noreply.github.com>
2023-01-26 13:46:27 +0400

    add

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -16,3 +16,24 @@ export function Buttons() {
         </div>
     )
 }  
+
+
+
+/* import { useState } from 'react';
+
+export function Buttons() {
+    const [count, setCount] = useState(0)
+    function countMount(){
+        setCount(count + 1)
+    }
+    return (
+        <div className="info-container">
+            <h3 style={{color: "black"}}>COUNTER</h3>
+            <h1 style={{color: "black"}}>{count}</h1>
+            <button onClick={()=> {setCount(count - 1)}}>lower count</button>
+            <button onClick={countMount}>add count</button>
+            <button onClick={()=> {setCount(0)}}>Reset</button>
+
+        </div>
+    )
+}   */
